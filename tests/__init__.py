b5390b8c98926f8f2d0d11fc6923119aa17911c4
Stephan Kaminsky <thaumicmekanism@gmail.com>
2019-08-27 19:25:35 -0700

    Generalized the import of python packages in that folder

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,8 @@
-from .Test1 import Test1
-from .Test2 import Test2
+import os
+import importlib
+#The exclude list is any python file you do not want to include.
+exclude_list = []
+for r, d, f in os.walk(__file__[:-len("__init__.py")]):
+    for file in f:
+        if file.endswith(".py") and file[:-3] not in exclude_list:
+            importlib.import_module(".{}".format(file[:-3]), package="tests")
