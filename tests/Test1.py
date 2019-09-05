8353de3be70c780cdb5abc9acdd68b81ef380a5c
Stephan Kaminsky <thaumicmekanism@gmail.com>
2019-09-04 19:20:28 -0700

    Added timeouts to tests

--- tests/Test1.py
+++ tests/Test1.py
@@ -1,6 +1,8 @@
 from GradescopeBase import AutograderTest, Max, Test
 
-@Test("Test 1: First test", 2)
+@Test("Test 1: First test", 2, timeout=1)
 def Test1_fn(ag, test: AutograderTest):
     test.print("This is the first test! It will always pass :)")
+    import time
+    time.sleep(10)   
     return Max()
--- tests/Test2.py
+++ tests/Test2.py
@@ -2,7 +2,7 @@ from GradescopeBase import AutograderTest, Max
 
 def Test2_fn(ag, test: AutograderTest):
     test.print("Second test!")
-    test.print("It only gives half points.")
+    test.print("It only gives half points.") 
     return test.max_score / 2
 
 Test2 = AutograderTest(Test2_fn, "Test 2: Second Test - Partial", 2)
