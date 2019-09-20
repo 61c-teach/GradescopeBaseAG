2952dae0cc0c47edb3629550885626329a4cd87c
Stephan Kaminsky <thaumicmekanism@gmail.com>
2019-09-19 22:21:39 -0700

    Updated to being in sync with fixes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+set -e
 
-mkdir -p /root/.ssh
-cat ssh_config >> /root/.ssh/config
-cp GradescopeBase_deploy_key /root/.ssh/GradescopeBase_deploy_key
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "=====Setting up the autograder====="
+
+echo "Initializing and updaing submodules..."
 git submodule update --init --recursive
 
+echo "Installing apt dependencies..."
 apt-get install -y python3 python3-pip python3-dev
-pip3 install -r /autograder/root/requirements.txt
+
+echo "Installing python3 dependencies..."
+pip3 install -r $DIR/requirements.txt
+
+echo "=====DONE!====="
