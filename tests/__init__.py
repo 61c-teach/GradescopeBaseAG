36ee8b29de2c506184bfa08dc34a2e7803109327
Stephan Kaminsky <thaumicmekanism@gmail.com>
2019-09-24 10:03:23 -0700

    Updated the order of the __init__

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,14 @@
 import os
 import importlib
 #The exclude list is any python file you do not want to include.
-exclude_list = []
+exclude_list = ["lib.py"]
 for r, d, f in os.walk(__file__[:-len("__init__.py")]):
-    for file in f:
+    for file in sorted(f, reverse=True):
         if file.endswith(".py") and file[:-3] not in exclude_list:
-            importlib.import_module(".{}".format(file[:-3]), package="tests")
+            try:
+                importlib.import_module(".{}".format(file[:-3]), package="tests")
+            except Exception as e:
+                print("Could not add a test file!")
+                import traceback
+                traceback.print_exc()
+                print(e)
