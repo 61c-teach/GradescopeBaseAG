6080731a1f091b607d923998a78b1788f0942843
Stephan Kaminsky <thaumicmekanism@gmail.com>
2019-09-30 13:51:41 -0700

    Added ratelimit example

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from GradescopeBase import Autograder, global_tests
+from GradescopeBase import Autograder, RateLimit, global_tests
 try:
     from tests import *
 except Exception as exc:
     print("Failed to import a test!")
     print(exc)
 try:
+    """
+    Here is an example of how to use the ratelimit.
+    We are giving students 3 tokens where a token will
+    take 12 hours to regenerate. If you do not want to rate
+    limit, you can replace the following three lines with:
     Autograder.run()
+    """
+    rate_limit = RateLimit(3, hours=12)
+    rate_limit_ag = Autograder(rate_limit)
+    Autograder.run(rate_limit_ag)
 except Exception as exc:
     print("Failed to run the autograder")
     print(exc)
