a937bb2ab0c21b57284674207abe849fce8c6c20
Stephan Kaminsky <thaumicmekanism@gmail.com>
2021-10-05 18:37:01 -0700

    Loaded GradescopeBase from pypi instead of as a submodule

--- setup.sh
+++ setup.sh
@@ -3,16 +3,13 @@
 # * @Author: ThaumicMekanism [Stephan K.] 
 # * @Date: 2020-01-23 21:05:45 
 # * @Last Modified by:   ThaumicMekanism [Stephan K.] 
-# * @Last Modified time: 2020-01-23 21:05:45 
+# * @Last Modified time: 2021-10-05 6:36:23 
 # */
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "=====Setting up the autograder====="
-
-echo "Initializing and updaing submodules..."
-git submodule update --init --recursive
 
 echo "Installing apt dependencies..."
 apt-get install -y python3 python3-pip python3-dev
